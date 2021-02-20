03acd1855e1f5ca958cbb72e7913a3fe57f097a8
xartol <xartol.dev@gmail.com>
2021-02-20 14:54:59 -0500

    adds registration and test cases
    adds registration screen and format
    adds registration test

--- project/app/Actions/Fortify/PasswordValidationRules.php
+++ project/app/Actions/Fortify/PasswordValidationRules.php
@@ -13,6 +13,13 @@ trait PasswordValidationRules
      */
     protected function passwordRules()
     {
-        return ['required', 'string', new Password, 'confirmed'];
+        // requires input, min length of 8, max length of 100, and at least 1 lowercase, 1 uppercase, 1 number, and 1 special character
+        return ['required',
+            'min:8',
+            'max:100',
+            'string',
+            'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/',
+            new Password,
+            'confirmed'];
     }
 }
--- project/tests/Feature/RegistrationTest.php
+++ project/tests/Feature/RegistrationTest.php
@@ -11,24 +11,170 @@ class RegistrationTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_registration_screen_can_be_rendered()
+    public function testRegistrationScreenRenders()
     {
         $response = $this->get('/register');
 
         $response->assertStatus(200);
     }
 
-    public function test_new_users_can_register()
+    public function testNewUsersCanRegister()
     {
+        // assemble correct user information
         $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
+            'name' => 'Testing Testerson',
+            'email' => 'testymcnotanemail@example.com',
+            'password' => 'impenetrableA1!',
+            'password_confirmation' => 'impenetrableA1!',
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
         ]);
 
+        // assert and validate success via authentication and page redirects to home
         $this->assertAuthenticated();
         $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function testNotAnEmail()
+    {
+        // assemble user input with an incorrect email that doesnt have a domain
+        $response = $this->post('/register', [
+            'name' => 'Testing Testerson',
+            'email' => 'testymcnotanemail',
+            'password' => 'impenetrableA1!',
+            'password_confirmation' => 'impenetrableA1!',
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        // assert not authenticated as a user and that a redirect to the homepage didnt happen
+        $this->assertGuest();
+        $response->assertNotRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function testEmailTooLong()
+    {
+        // assemble user input with incorrect email that is too long
+        $response = $this->post('/register', [
+            'name' => 'Testing Testerson',
+            'email' => 'testymcnotanemailisanemailthatneverendsitgoesonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonandonanduntiitistoobig@example.com',
+            'password' => 'impenetrableA1!',
+            'password_confirmation' => 'impenetrableA1!',
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        // assert not authenticated as a user and that a redirect to the homepage didnt happen
+        $this->assertGuest();
+        $response->assertNotRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function testNameTooLong()
+    {
+        // assemble user input with too long name
+        $response = $this->post('/register', [
+            'name' => 'Test User is the name that doesn’t end. It goes on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on and on until it is too big',
+            'email' => 'Testing Testerson',
+            'password' => 'impenetrableA1!',
+            'password_confirmation' => 'impenetrableA1!',
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        // assert not authenticated as a user and that a redirect to the homepage didnt happen
+        $this->assertGuest();
+        $response->assertNotRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function testDifferentPasswordConfirmation()
+    {
+        // assemble user input with a different password in confirmation from the original
+        $response = $this->post('/register', [
+            'name' => 'Testing Testerson',
+            'email' => 'testymcnotanemail@example.com',
+            'password' => 'impenetrableA1!',
+            'password_confirmation' => 'infinite22FAILS!',
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        // assert not authenticated as a user and that a redirect to the homepage didnt happen
+        $this->assertGuest();
+        $response->assertNotRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function testPasswordTooShort()
+    {
+        // assemble user input with a password that is too short
+        $response = $this->post('/register', [
+            'name' => 'Testing Testerson',
+            'email' => 'testymcnotanemail@example.com',
+            'password' => 'short',
+            'password_confirmation' => 'short',
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        // assert not authenticated as a user and that a redirect to the homepage didnt happen
+        $this->assertGuest();
+        $response->assertNotRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function testPasswordTooLong()
+    {
+        // assemble user input with a password that is too long
+        $response = $this->post('/register', [
+            'name' => 'Testing Testerson',
+            'email' => 'testymcnotanemail@example.com',
+            'password' => 'impenetrableA1!isthepasswordthatneverendsitgoesonandonandonandonandonandonandonandonandonandonandonandonandonuntilitistoolong',
+            'password_confirmation' => 'impenetrableA1!isthepasswordthatneverendsitgoesonandonandonandonandonandonandonandonandonandonandonandonandonuntilitistoolong',
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        // assert not authenticated as a user and that a redirect to the homepage didnt happen
+        $this->assertGuest();
+        $response->assertNotRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function testPasswordNoUppercase()
+    {
+        // assemble user input with a password that has no uppercase letters
+        $response = $this->post('/register', [
+            'name' => 'Testing Testerson',
+            'email' => 'testymcnotanemail@example.com',
+            'password' => 'impenetrable1!',
+            'password_confirmation' => 'impenetrable1!',
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        // assert not authenticated as a user and that a redirect to the homepage didnt happen
+        $this->assertGuest();
+        $response->assertNotRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function testPasswordNoLowercase()
+    {
+        // assemble user input with a password that has no lower case characters
+        $response = $this->post('/register', [
+            'name' => 'Testing Testerson',
+            'email' => 'testymcnotanemail@example.com',
+            'password' => 'IMPENETRABLEA1!',
+            'password_confirmation' => 'IMPENETRABLEA1!',
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        // assert not authenticated as a user and that a redirect to the homepage didnt happen
+        $this->assertGuest();
+        $response->assertNotRedirect(RouteServiceProvider::HOME);
+    }
+
+    public function testPasswordNoSymbol()
+    {
+        // assemble user input with a password that has no symbols
+        $response = $this->post('/register', [
+            'name' => 'Testing Testerson',
+            'email' => 'testymcnotanemail@example.com',
+            'password' => 'impenetrableA1',
+            'password_confirmation' => 'impenetrableA1',
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+        ]);
+
+        // assert not authenticated as a user and that a redirect to the homepage didnt happen
+        $this->assertGuest();
+        $response->assertNotRedirect(RouteServiceProvider::HOME);
     }
 }
