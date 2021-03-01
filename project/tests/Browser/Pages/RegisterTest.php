a705ad3953b3a21e9eaf19de92fe696f22211e83
Aidanthewiz <Aidanartherton@gmail.com>
2021-02-28 20:47:56 -0500

    Fixes spotify test issues

--- project/tests/Browser/Pages/RegisterTest.php
+++ project/tests/Browser/Pages/RegisterTest.php
@@ -101,7 +101,7 @@ class RegisterTest extends DuskTestCase
         // assert that the spotify link is visible
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                ->assertSeeLink('/oauth/spotify');
+                ->assertVisible('@spotify-link');
         });
     }
 }
